341db435949a000ff0c7b168ac73845eb52cbf2c
Juan Zabala <zabala1juan@gmail.com>
2023-12-09 21:50:48 -0500

    Consideraciones Produccion y CORS

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express'); //importamos express
 //importamos el router, el index.js de la carpeta routes
 const routerApiTraida = require('./routes');
 
+const cors = require('cors');
+
 //vamos a importar los middlewares
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler');
 
@@ -15,6 +17,26 @@ const port = 3000;
 //para poder usar le metodo POST, debemos agregar este middle work
 //con este ajuste ya deberíamos recibir información que llega por POST
 app.use(express.json())
+
+
+//llamamos el middleware cors
+//crearemos un array con los origenes validos
+const whitelist = ['http://localhost:3000','http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+
+    // if (whitelist.includes(origin)){ //esta opcion no permite acceso desde el same-origin
+    if (!origin || whitelist.includes(origin)){
+      //null indica que no hay ningun error
+      //true, el acceso esta permitido
+      callback(null, true);
+    } else {
+        callback(new Error('no permitido'));
+    }
+  }
+}
+// app.use(cors()); //aquí estaríamos habilitando cualquier dominio
+app.use(cors(options));
 
 
 //vamos a definir una ruta
@@ -29,6 +51,9 @@ app.get('/nueva-ruta',(req,res)=> {
 });
 //estamos modularizando la aplicación, cada ruta tiene un modelo
 routerApiTraida(app);
+
+
+
 
 //los midllewares deben estar despues del router, el orden es relevante
 app.use(logErrors);
